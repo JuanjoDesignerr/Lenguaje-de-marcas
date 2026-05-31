b2e40a1ff719d2252b97f49434dde9495c017503
Juanjo Gea <geacorreasjuanjo@gmail.com>
2026-06-01 01:37:21 +0200

    Usuarios administradores tienen acceso tanto a login normal como parte admin.

--- admin.php
+++ admin.php
@@ -4,27 +4,27 @@
     try {
         $pdo = new PDO("mysql:host=localhost;dbname=poli_bd;charset=utf8", "root", "");
         
-        if (isset($_POST['admin_nombre']) && isset($_POST['admin_contraseña'])) {
+        if (isset($_POST['admin_nombre']) && isset($_POST['admin_contrasena'])) {
             $nombre = $_POST['admin_nombre'];
-            $pass   = $_POST['admin_contraseña'];
+            $pass = $_POST['admin_contrasena'];
             
             // Buscamos al usuario por su nombre
             $stmt = $pdo->prepare('SELECT * FROM usuarios WHERE nombre = :nombre');
             $stmt->execute([':nombre' => $nombre]);
-
+           
+            // Extrae el registro encontrado como un array asociativo (clave-valor). Si no hay coincidencia, devuelve false.
             $usuarioLogueado = $stmt->fetch(PDO::FETCH_ASSOC);
-        }
-        
 
-        // Si el usuario existe, la contraseña coincide Y además su rol es 'admin'
-        if ($usuarioLogueado && $pass == $usuarioLogueado['contrasena'] && $usuarioLogueado['rol'] == 'admin') {
-            
-            // Si cumple las TRES cosas a la vez, guardamos sesión y entra
-            $_SESSION['admin_nombre'] = $usuarioLogueado['nombre'];
-            header("Location: panel_control.html"); 
-            exit();
-        } else {
-            $error = "Error el usuario o la contraseña no son correctos, revise sus permisos.";
+            // Si el usuario existe, la contraseña coincide Y además su rol es 'admin'
+            if ($usuarioLogueado && $pass == $usuarioLogueado['contrasena'] && $usuarioLogueado['rol'] == 'admin') {
+                
+                // Si cumple las TRES cosas a la vez, guardamos sesión y entra
+                $_SESSION['admin_nombre'] = $usuarioLogueado['nombre'];
+                header("Location: gestionUsuarios.html"); 
+                exit();
+            } else {
+                $error = "Error el usuario o la contraseña no son correctos, revise sus permisos.";
+            }
         }
 
     //Excepción en caso de que la conexion no se realize.
@@ -32,7 +32,6 @@
         $error = "Error conectando con la base de datos:" . $e->getMessage();
     }
      
-
 ?>
 
 <!DOCTYPE html>
@@ -58,23 +57,26 @@
         <div id="contenedor">
             <h2>ACCESO ADMINISTRADOR</h2>
 
+            <?php if (!empty($error)) {
+                    echo $error; 
+                    }   
+                ?>
+
             <div class="campo">
                 <label>USUARIO:</label>
-                <input type="text" name="admin_nombre" placeholder="Usuario administrador">
+                <input type="text" name="admin_nombre" placeholder="Usuario administrador" required>
             </div>
 
             <div class="campo">
                 <label>CONTRASEÑA:</label>
-                <input type="password" name="admin_contrasena" placeholder="Contraseña">
+                <input type="password" name="admin_contrasena" placeholder="Contraseña" required>
             </div>
 
-            <button type="submit" class="boton_login">ENTRAR AL PANEL</button>
-            <a href="Login.html" class="enlace">← Volver al login</a>
+            <button type="submit" class="boton">ENTRAR AL PANEL</button>
+            <a href="Login.php" class="enlace">Volver al login</a>
         </div>
     </form>
 
 </body>
 </html>
 
-</body>
-</html>
